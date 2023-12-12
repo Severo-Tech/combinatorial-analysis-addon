738d244a1be54ca2b3cd68a74dee2c49ccb9fbe6
Guilherme Severo <guilherme.severo@somma-it.com>
2023-12-12 16:08:33 -0300

    Polish

--- src/addon/combinations.c
+++ src/addon/combinations.c
@@ -5,16 +5,14 @@
 
 #include "combinations.h"
 
+const int MAX_ALLOWED_COMBINATIONS = 10000;
+const double FLOAT_ACCEPTABLE_DIFF = 0.001;
+
 int compareByValue(const void *a, const void *b) {
     double aValue = ((ICombinationsValue *)a)->value;
     double bValue = ((ICombinationsValue *)b)->value;
 
     return bValue - aValue;
-}
-
-int isDoubleEquals(double a, double b) {
-    double epsilon = 0.001;
-    return fabs(a - b) < epsilon;
 }
 
 Combination addElementToCombination(const Combination* current, const ICombinationsValue* element) {
@@ -31,16 +29,36 @@ Combination addElementToCombination(const Combination* current, const ICombinati
     return newCombination;
 }
 
-double getListSum(const ICombinationsValue* list, int listSize) {
-    double sum = 0;
-    for (int i = 0; i < listSize; ++i) {
-        sum += list[i].value;
+int validateRemainingList(const ICombinationsValue* remaining, int remainingSize, double remainingValue) {
+    // Filtrar elementos que cabem na soma
+    int innerRemainingSize = 0;
+    for (int i = 0; i < remainingSize; ++i) {
+        if (remaining[i].value <= remainingValue) {
+            innerRemainingSize++;
+        }
     }
-    return sum;
+    if (innerRemainingSize == 0) {
+        return 0;
+    }
+
+    // Verificar se a soma dos restantes pode combinar com o target
+    double sumOfRemaining = 0;
+    for (int i = 0; i < remainingSize; ++i) {
+        sumOfRemaining += remaining[i].value;
+    }
+    if (sumOfRemaining < remainingValue) {
+        return 0;
+    }
+
+    return 1;
 }
 
 void findCombination(const Combination* current, const ICombinationsValue* remaining, int remainingSize, double remainingValue, Combination** combinations, int* combinationsCount) {
-    if (isDoubleEquals(remainingValue, 0.00)) {
+    if (*combinationsCount >= MAX_ALLOWED_COMBINATIONS) {
+        return;
+    }
+
+    if (fabs(remainingValue - 0.00) < FLOAT_ACCEPTABLE_DIFF) {
         *combinations = realloc(*combinations, (*combinationsCount + 1) * sizeof(Combination));
         (*combinations)[*combinationsCount] = *current;
         ++(*combinationsCount);
@@ -51,34 +69,19 @@ void findCombination(const Combination* current, const ICombinationsValue* remai
         return;
     }
 
-    // Filtrar elementos que cabem na soma
-    ICombinationsValue* innerRemaining = malloc(remainingSize * sizeof(ICombinationsValue));
-    int innerRemainingSize = 0;
-    for (int i = 0; i < remainingSize; ++i) {
-        if (remaining[i].value <= remainingValue) {
-            innerRemaining[innerRemainingSize++] = remaining[i];
-        }
-    }
-
-    if (innerRemainingSize == 0) {
-        free(innerRemaining);
+    if (validateRemainingList(remaining, remainingSize, remainingValue) != 1) {
         return;
     }
 
-    double sumOfRemaining = getListSum(innerRemaining, innerRemainingSize);
-    if (sumOfRemaining < remainingValue) {
-        free(innerRemaining);
-        return;
-    }
-
-    ICombinationsValue invoice = innerRemaining[0];
+    ICombinationsValue invoice = remaining[0];
     double invoiceValue = invoice.value;
 
     Combination newCurrent = addElementToCombination(current, &invoice);
+
     findCombination(
         &newCurrent,
-        innerRemaining + 1,
-        innerRemainingSize - 1,
+        remaining + 1,
+        remainingSize - 1,
         remainingValue - invoiceValue,
         combinations,
         combinationsCount
@@ -86,15 +89,12 @@ void findCombination(const Combination* current, const ICombinationsValue* remai
 
     findCombination(
         current,
-        innerRemaining + 1,
-        innerRemainingSize - 1,
+        remaining + 1,
+        remainingSize - 1,
         remainingValue,
         combinations,
         combinationsCount
     );
-
-    // Liberar a memória alocada dinamicamente
-    free(innerRemaining);
 }
 
 Combination* combineByValue(ICombinationsValue* list, int listSize, double target, int* combinationsCount) {
@@ -106,17 +106,9 @@ Combination* combineByValue(ICombinationsValue* list, int listSize, double targe
 
     qsort(list, listSize, sizeof(ICombinationsValue), compareByValue);
 
-    clock_t start;
-
-    start = clock();
+    clock_t start = clock();
     findCombination(&current, list, listSize, target, &combinations, combinationsCount);
-
-    // for (int i = 0; i < *combinationsCount; ++i) {
-    //     printCombination(&combinations[i]);
-    // }
-
     printf("Tempo de execução: %f segundos\n", ((double) (clock() - start)) / CLOCKS_PER_SEC);
-    // printf("combinações: %d\n", *combinationsCount);
 
     return combinations;
 }
